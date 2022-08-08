cb64e41e8ca6674da1c101790d0fecc4d402dd9e
Alex Topunov <telefoniero@github.com>
2022-08-08 21:34:10 +0300

    level 4 is done

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    boolean flag;
+    short smallNum;
+    int integerNum;
+    long longNum;
+    byte extraSmallNum;
+    float floatNum;
+    char character;
+    double bigFloat;
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String str1 = new String("asdf");
+        String str2 = new String("asdf");
+        String str3 = new String("asdf");
+        String str4 = new String("asdf");
+        String str5 = new String("asdf");
+        String str6 = new String("asdf");
+        String str7 = new String("asdf");
+        String str8 = new String("asdf");
+        String str9 = new String("asdf");
+        String str10 = new String("asdf");
+
+        int[] arr1 = new int[1];
+        int[] arr2 = new int[1];
+        int[] arr3 = new int[1];
+        int[] arr4 = new int[1];
+        int[] arr5 = new int[1];
+
+        Scanner scanner1 = new Scanner(System.in);
+        Scanner scanner2 = new Scanner(System.in);
+    }
+}
